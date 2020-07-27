58f61dd65e2069644fb6812be977f909f38758b5
Thezigzagtree <33034701+Thezigzagtree@users.noreply.github.com>
2020-07-27 10:56:01 +0300

    Changed URLs

--- fm120k_db/serializers.py
+++ fm120k_db/serializers.py
@@ -10,5 +10,5 @@ class SampleListSerializer (serializers.ModelSerializer):
 class SampleCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = SeawaterSample
-        fields = ['sampleLabel', 'depth']
+        fields = ['sampleLabel', 'depth',]
         ##fields = ['sampleNumber', 'sampleLabel', 'sampleStation', 'collectionTiming', 'sampleVolume', 'processed', 'storage']
