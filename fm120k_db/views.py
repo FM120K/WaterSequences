ee7f5bd54e9e6135bce00f6319311ea2baf19fbb
Thezigzagtree <33034701+Thezigzagtree@users.noreply.github.com>
2020-10-04 12:45:33 +0300

    Reverted to og

--- fm120k_db/views.py
+++ fm120k_db/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import SeawaterSample
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
-from rest_framework.views import APIView
 from .serializers import SampleListSerializer, SampleCreateSerializer
 # Create your views here.
 
@@ -11,18 +10,9 @@ class sampleList(ListAPIView):
     serializer_class = SampleListSerializer
 
 
-class SampleCreate (APIView):
+class SampleCreate (CreateAPIView):
     serializer_class = SampleCreateSerializer
-
-    def post (self, request, *args, **kwargs):
-        serializer = SampleSerializer(data=request.data)
-
-        if serializer.is_valid(raise_exception=True):
-            Sample.objects.create(sampleNumber=request.data["sampleNumber"],sampleVolume=request.data["sampleVolume"], depth = request.data["depth"], sampleLabel = request.data["sampleLabel"], sampleStation = request.data["sampleStation"], storage= request.data["storage"], processed = request.data["processed"], extractionKit = request.data["extractionKit"])
-
-            return Response("Data is inserted successfully", status=status.HTTP_200_OK)
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-     
+    
 
 
 def sample_detail(request, sample_id):
